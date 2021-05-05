7680320a2cb578534c009c2061357dd0b880e5a4
DPLATA <diegogplata@gmail.com>
2021-05-05 16:49:52 -0500

    🎉✨add a new view✨add new route in router file

---
+++ src/views/Commits.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="commits">
+      <h1>Hello commits</h1>
+      <!-- v-for every commit should be here -->
+      <ListElement comment="my commits in X repo"/>
+      <ul>
+          <li v-for="(fruit, index) in fruits" v-bind:key="index">{{index}}, {{fruit}}</li>
+      </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ListElement from "@/components/ListElement.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Commits",
+  components: {
+    ListElement,
+  },
+  data() {
+      return {
+          fruits: ['apple', 'banana', 'orange', 'grapes']
+      }
+  }
+});
+</script>
