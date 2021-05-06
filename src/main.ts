59f63ce7a120542694028df6e19c82ed3fdd3632
DPLATA <diegogplata@gmail.com>
2021-05-05 20:55:18 -0500

    ➕setup axios, vue-axios in main file

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios"
 
-createApp(App).use(store).use(router).mount("#app");
+axios.defaults.baseURL = 'https://api.github.com'
+createApp(App).use(store).use(router).use(VueAxios, axios).mount("#app");
