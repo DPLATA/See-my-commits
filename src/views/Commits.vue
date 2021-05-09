0f76a316137ad7498954065ecc9e1fd2bf033e37
DPLATA <diegogplata@gmail.com>
2021-05-08 21:05:22 -0500

    ✨update commits view file

--- src/views/Commits.vue
+++ src/views/Commits.vue
@@ -2,40 +2,38 @@
   <div class="commits">
       <h1>Hello commits</h1>
       <!-- v-for every commit should be here -->
-      <ListElement comment="my commits in X repo"/>
-      <ul>
-          <li v-for="(fruit, index) in fruits" v-bind:key="index">{{index}}, {{fruit}}</li>
-      </ul>
+      <CommitList :commits="commits" />
       <button v-on:click="get_github_api">Github zen</button>
-      <div v-if="zen_message_available">{{zen_message}}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import ListElement from "@/components/ListElement.vue"; // @ is an alias to /src
+import { defineComponent, reactive, ref } from "vue";
+import CommitList from "@/components/CommitList.vue"; // @ is an alias to /src
+import Commit from "@/types/Commit";
 
 export default defineComponent({
   name: "Commits",
   components: {
-    ListElement,
+    CommitList,
   },
-  data() {
-      return {
-          fruits: ['apple', 'banana', 'orange', 'grapes'],
-          zen_message: null,
-          zen_message_available: false
-      }
+  setup () {
+    const commits = ref<Commit[]>([
+      { sha: '0948d99', message: 'First commit'},
+      { sha: 'bc2918f', message: 'Mock commits array'},
+      { sha: 'df8f179', message: 'Vue app'}
+
+    ])
+    return { commits }
   },
   methods: {
     async get_github_api() {
-      await this.axios.get('/zen')
+      await this.axios.get('/repos/DPLATA/See-my-commits/commits')
       .then(response => {
-          this.zen_message_available = true
-          this.zen_message = response.data
-          console.log(response)
+          let {data} = response
+          console.log(data)
       }).catch(error => {
-        var data = error.response.data
+        console.log('error')
       });
     }
   }
