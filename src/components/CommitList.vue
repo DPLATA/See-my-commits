ded1f598a9858f527a6b268faae2422a7a192772
DPLATA <diegogplata@gmail.com>
2021-05-08 21:04:43 -0500

    ♻️refactor list element component to commit list component

---
+++ src/components/CommitList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="commit_list">
+      <ul>
+        <li v-for="commit in commits" :key="commit.sha">
+          <h3>Commit {{commit.sha}}</h3>
+          <div>
+            <p>with message: {{commit.message}}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Commit from "@/types/Commit";
+
+export default defineComponent({
+  name: "CommitList",
+  props: {
+    commits: {
+      required: true,
+      type: Array as PropType<Commit[]>
+    }
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  // display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
